6eae7ec511bb6c01048b08fa794208ff6b36eb0b
fedotick <feodortcaciuc2004@gmail.com>
2023-11-19 22:50:31 +0200

    Created tables for products and categories

---
+++ src/main/java/md/fedot/magaz/domain/Category.java
@@ -0,0 +1,40 @@
+package md.fedot.magaz.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/md/fedot/magaz/domain/Product.java
@@ -0,0 +1,55 @@
+package md.fedot.magaz.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false, precision = 8, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false, columnDefinition = "smallint")
+    private Short quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    @Column(nullable = false)
+    private Category category;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
