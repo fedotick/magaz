cc2e2fb8523350bf16ab1ba21021c00269752c42
fedotick <feodortcaciuc2004@gmail.com>
2023-11-19 19:49:29 +0200

    Project created by Spring Initializr

---
+++ src/main/java/md/fedot/magaz/MagazApplication.java
@@ -0,0 +1,13 @@
+package md.fedot.magaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/md/fedot/magaz/MagazApplicationTests.java
@@ -0,0 +1,13 @@
+package md.fedot.magaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
